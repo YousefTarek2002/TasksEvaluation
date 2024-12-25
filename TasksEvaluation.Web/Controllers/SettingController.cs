2c4acbec6ed5a207dbe0f1d64ce2244050e3e1a3
youssef <youssef.alkholy1700137@science.menofia.edu.eg>
2024-12-25 22:22:47 +0200

    dth

---
+++ TasksEvaluation.Web/Controllers/SettingController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TasksEvaluation.Web.Controllers
+{
+    public class SettingsController : Controller
+    {
+        public IActionResult Index()
+        {
+            // يمكن استخدام نموذج (Model) لتمرير البيانات بدل ViewBag
+            ViewBag.UserSettings = new
+            {
+                Username = "JohnDoe",
+                Email = "johndoe@example.com",
+                EmailNotifications = true,
+                SMSNotifications = false
+            };
+            return View();
+        }
+    }
+}
